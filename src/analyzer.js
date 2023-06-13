5485654aa969d37db356f9d6eb073631996b48cc
Nancy A Contreras <nancyacontreras@Nancys-MacBook-Pro.local>
2023-06-12 22:08:54 -0500

    nuevo html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,101 @@
 const analyzer = {  
+
+  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text.split(' ').length; //metodo slip toma una cadena como argumento y devuelve una matriz de subcadenas. La matriz contiene todas las palabras de la cadena original
+  //length devuelve un numero entero que representa el numero de palabras en la cadena                     
   },
+  // console.log('Primer resultado: ' + text)
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length; 
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /\S/g; //Es una expresión regular donde \s significa "coincidir con espacios en blanco" 
+    //y g es una bandera que significa "global", es decir, coincide con todos los 
+    //espacios en blanco, no solo con el primero.
+    
+    return text.split(regex).length - 1;
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const aveLeng = text.split(' ');
+    
+    let sumaAveLeng = 0;
+    for(let i=0; i<aveLeng.length; i++){
+      sumaAveLeng += aveLeng[i].length
+    }
+    return Math.round(sumaAveLeng/aveLeng.length);
+    
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    /*const cadena = text.replace(/[^0-9]+/g, "");
+    console.log(cadena)
+    return cadena.length;*/
+
+    /*const cadena = text.replace(/[^0-9]+/g, "");
+    const resul = cadena.split('');
+    console.log(resul)
+    return cadena.length;*/
+
+    let countNumber = 0;
+    //const cadena = text.replace(/[^0-9]+/g, "");
+    const resul = text.split(' '); // [HOLA12, 12, 3, CHAO]
+    //console.log(countNumber+resul)
+    
+
+    for(let i=0; i<(resul.length); i++){
+      // if(typeof cadena[i]!== 'numero' || !Number.isInteger(cadena[i])){  
+      if (resul[i] === '') {
+        continue;
+      }  
+      if(!isNaN(resul[i])){
+        //if(isNaN(cadena[i])){
+        countNumber= countNumber+1;
+      }
+      
+    }
+    return countNumber;
+
+    //extrar el arreglo de caracteres con split
+    //recorrer el arreglo  con un for 
+    //dentro del for preguntar con la expresion regular si cada caracter  me devuelve un true
+    //si la expresion regular me da true aumenta el contador
+    //retornar contador
+    // console.log(expr.test(text))
+
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    
+    const cadenaN = text.split(' '); // [HOLA12, 12, 3, CHAO]
+    let sumNumber = 0;
+    for(let i=0; i<cadenaN.length; i++){ 
+      if(cadenaN[i] === '') {
+        continue;
+      }  
+      if(!isNaN(cadenaN[i])){
+        sumNumber = sumNumber + parseFloat(cadenaN[i]); //el dato lo convierte a entero
+      }
+    }
+    return sumNumber;
   },
+  
 };
 
 export default analyzer;
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textArea = document.querySelector('textarea[name="user-input"]'); /*obtener el texto del área de texto*/
+const metrics = document.querySelectorAll('li'); //trae toda la lista de nodos li
+
+
+textArea.addEventListener('keyup', function(){ // escucha el evento keyup del textarea, cuando se escribe en el cuadro de texto 
+
+  metrics[0].innerHTML = 'Word Count: ' + analyzer.getWordCount(textArea.value);
+  metrics[1].innerHTML = 'Character Count: ' + analyzer.getCharacterCount(textArea.value); //concatenacion del texto con el metodo getCharacterCount
+  metrics[2].innerHTML = 'Character Count Excluding Spaces: ' + analyzer.getCharacterCountExcludingSpaces(textArea.value);
+  metrics[3].innerHTML = 'Number Count: ' + analyzer.getNumberCount(textArea.value);
+  metrics[4].innerHTML = 'Number Sum: ' + analyzer.getNumberSum(textArea.value);
+  metrics[5].innerHTML = 'Average Word Length: ' + analyzer.getAverageWordLength(textArea.value);
+//console.log(textArea.value);
+})
+
+// CLEAR BUTTON //
+const button = document.getElementById("reset-button");
+button.addEventListener('click', function(){
+  textArea.value = '';
+  metrics[0].innerHTML = 'Word Cont: ';
+  metrics[1].innerHTML = 'Character Count: '; //establece el contenido de la posicion 0 a 'characters'
+})
